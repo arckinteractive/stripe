2681c350e2acc60bc2b87f6589d5b6dfd142e0c0
Matt Beckett <beck24@gmail.com>
2017-02-16 11:26:48 -0800

    fix(php notice): check for user entity before creating menu items

--- lib/events.php
+++ lib/events.php
@@ -15,41 +15,43 @@ function stripe_pagesetup() {
 
 	$user = elgg_get_page_owner_entity();
 
-	elgg_register_menu_item('page', array(
-		'name' => 'stripe:cards',
-		'href' => "billing/$user->username/cards/all",
-		'text' => elgg_echo('stripe:cards:all'),
-		'selected' => (substr_count(current_page_url(), 'cards/all')),
-		'context' => 'settings',
-		'section' => 'stripe',
-	));
+	if ($user) {
+		elgg_register_menu_item('page', array(
+			'name' => 'stripe:cards',
+			'href' => "billing/$user->username/cards/all",
+			'text' => elgg_echo('stripe:cards:all'),
+			'selected' => (substr_count(current_page_url(), 'cards/all')),
+			'context' => 'settings',
+			'section' => 'stripe',
+		));
 
-	elgg_register_menu_item('page', array(
-		'name' => 'stripe:charges',
-		'href' => "billing/$user->username/charges/all",
-		'text' => elgg_echo('stripe:charges:all'),
-		'selected' => (substr_count(current_page_url(), 'charges/all')),
-		'context' => 'settings',
-		'section' => 'stripe',
-	));
+		elgg_register_menu_item('page', array(
+			'name' => 'stripe:charges',
+			'href' => "billing/$user->username/charges/all",
+			'text' => elgg_echo('stripe:charges:all'),
+			'selected' => (substr_count(current_page_url(), 'charges/all')),
+			'context' => 'settings',
+			'section' => 'stripe',
+		));
 
-	elgg_register_menu_item('page', array(
-		'name' => 'stripe:invoices',
-		'href' => "billing/$user->username/invoices/all",
-		'text' => elgg_echo('stripe:invoices:all'),
-		'selected' => (substr_count(current_page_url(), 'invoices/all')),
-		'context' => 'settings',
-		'section' => 'stripe',
-	));
+		elgg_register_menu_item('page', array(
+			'name' => 'stripe:invoices',
+			'href' => "billing/$user->username/invoices/all",
+			'text' => elgg_echo('stripe:invoices:all'),
+			'selected' => (substr_count(current_page_url(), 'invoices/all')),
+			'context' => 'settings',
+			'section' => 'stripe',
+		));
 
-	elgg_register_menu_item('page', array(
-		'name' => 'stripe:subscriptions',
-		'href' => "billing/$user->username/subscriptions/all",
-		'text' => elgg_echo('stripe:subscriptions:all'),
-		'selected' => (substr_count(current_page_url(), 'subscriptions/all')),
-		'context' => 'settings',
-		'section' => 'stripe',
-	));
+		elgg_register_menu_item('page', array(
+			'name' => 'stripe:subscriptions',
+			'href' => "billing/$user->username/subscriptions/all",
+			'text' => elgg_echo('stripe:subscriptions:all'),
+			'selected' => (substr_count(current_page_url(), 'subscriptions/all')),
+			'context' => 'settings',
+			'section' => 'stripe',
+		));
+	}
 }
 
 /**
